dde23f515bb87a98106ce8928651d8946d090b49
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-07-31 17:48:35 +0200

    -

--- index.js
+++ index.js
@@ -1,113 +1,118 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const Note = require('./models/note')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Note = require("./models/note");
+const app = express();
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(requestLogger)
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(cors())
+app.use(requestLogger);
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (request, response) => {
-  response('<h1>hello</h1>')
-})
+app.get("/", (request, response) => {
+  response("<h1>hello</h1>");
+});
 
 //Get all the notes stored in the database onpageload
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
 //Get a specific user by their id
-app.get('/api/notes/:id', (request, response, next) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Notes.findById(request.params.id)
-    .then(note => {
+    .then((note) => {
       if (note) {
-        response.json(note)
+        response.json(note);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 //Deltee a specific user by their id
-app.delete('/api/notes/:id', (request, response, next) => {
+app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then((result) => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 //Add a new user to the database with the passed in content
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response, next) => {
+  const body = request.body;
 
-  if(body.content === undefined) {
+  if (body.content === undefined) {
     return response.status(404).json({
-      error: 'content missing'
-    })
+      error: "content missing",
+    });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
-  })
+    date: new Date(),
+  });
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
+});
 
 //Edit the data of a speicific person give the id (toggle importance)
-app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
   const note = {
     content: body.content,
-    important: body.important
-  }
+    important: body.important,
+  };
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-  .then(updateNote => {
-    response.json(updateNote)
-  })
-  .catch(error => next(error))
-})
+    .then((updateNote) => {
+      response.json(updateNote);
+    })
+    .catch((error) => next(error));
+});
 
 //When the user requests an non-existing route call this function to respond with a 404 eror
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 //When other shit goes wrong this errorHandler can be called with the next() method in any other route
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if ( error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
